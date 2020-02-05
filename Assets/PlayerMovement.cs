76af40864db63c00af6f7ee6e8d561aa1491e376
Riku <rivater@gmail.com>
2020-02-05 14:07:13 +0200

    tuli sillai lisättyy tavaraa

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    public float jumpForce = 2000f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+       if ( Input.GetKey("space"))
+       {
+          rb.AddForce(0, jumpForce * Time.deltaTime, 0);
+       }
+
+        
+        if ( Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if ( Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+
+        if ( Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0,forwardForce * Time.deltaTime);
+        }
+
+        if ( Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+    }
+
+}
